405c11952f15cdb8bb6abfd08b2af434af24c560
clayton.castro <cocastro@apoioprodesp.sp.gov.br>
2024-01-26 16:03:25 -0300

    Primeiro commit

---
+++ src/main/java/com/project/pontointeresse/PontoInteresseApplication.java
@@ -0,0 +1,13 @@
+package com.project.pontointeresse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoInteresseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteresseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/pontointeresse/PontoInteresseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.pontointeresse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteresseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
